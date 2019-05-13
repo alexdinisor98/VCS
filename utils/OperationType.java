31a9444160335809b783e82dc84472bd699a1743
alexdinisor98 <50598954+alexdinisor98@users.noreply.github.com>
2019-05-14 01:10:15 +0300

    Add files via upload

---
+++ utils/OperationType.java
@@ -0,0 +1,21 @@
+package utils;
+
+public enum OperationType {
+    // TODO add vcs commands
+    ROLLBACK,
+    COMMIT,
+    LOG,
+    VCS_INVALID_OPERATION,
+    STATUS,
+    BRANCH,
+    CHECKOUT,
+    CAT,
+    CHANGEDIR,
+    LIST,
+    MAKEDIR,
+    REMOVE,
+    TOUCH,
+    WRITETOFILE,
+    PRINT,
+    FILESYSTEM_INVALID_OPERATION
+}
